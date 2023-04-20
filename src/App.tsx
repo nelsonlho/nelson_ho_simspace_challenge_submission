3e72d26017672ae7c638d6aeb2371f48746eebaf
nelsonlho <nelsonlho@gmail.com>
2023-04-19 18:13:56 -0700

    set up calls to dogs api, showing buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useRef } from 'react';
+import { useFetch } from './common/hooks';
+import { LIMIT } from './common/constants';
+
+type BreedsMessage = {
+  message: {
+    [key: string]: string[];
+  };
+};
 
 function App() {
+  const isComponentMounted = useRef(true);
+
+  const { data, loading, error } = useFetch<BreedsMessage>({
+    url: `https://dog.ceo/api/breeds/list/all`,
+    ref: isComponentMounted,
+  });
+
+  const breeds = useMemo(() => {
+    return data
+      ? Object.keys(data.message)
+          .filter((_, index) => index < LIMIT)
+          .sort((previous, latter) => (previous > latter ? 1 : -1))
+      : null;
+  }, [data]);
+
+  type BreedsType = {
+    breeds: string[];
+  };
+
+  const Breeds = ({ breeds }: BreedsType) => {
+    return (
+      <div className="container m-auto grid grid-cols-4 gap-1">
+        {breeds.map((breed, index) => (
+          <div
+            className="cursor-pointer capitalize bg-gray-200 hover:bg-gray-400 justify-center text-center my-1 mx-4 p-4 "
+            key={`${index}_${breed}`}
+          >
+            {breed}
+          </div>
+        ))}
+      </div>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="flex items-center justify-between flex-wrap my-2 mx-4">
+        <div>Dogs!!</div>
+        <div>
+          <input
+            className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search breed"
+          />
+        </div>
+      </div>
+      <div>
+        {loading ? 'Loading...' : null}
+        {error ? 'An error has occurred' : null}
+        {!loading && !error && breeds ? <Breeds breeds={breeds} /> : null}
+      </div>
+    </>
   );
 }
 
---
+++ src/common/hooks.ts
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+
+interface UseFetchProps<T> {
+  url: string;
+  ref: React.MutableRefObject<boolean>;
+}
+
+interface UseFetchReturn<T> {
+  loading: boolean;
+  data: T | null;
+  error: string | null;
+}
+
+export const useFetch = <T>({
+  url,
+  ref,
+}: UseFetchProps<T>): UseFetchReturn<T> => {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (ref.current) {
+      (async () => {
+        try {
+          const res = await fetch(url);
+          const resJson = await res.json();
+          setData(resJson);
+          setError(null);
+        } catch (err) {
+          setError(`Problem fetching ${url}`);
+        } finally {
+          setLoading(false);
+        }
+      })();
+    }
+    return () => {
+      ref.current = false;
+    };
+  }, [url, ref]);
+
+  return { loading, data, error };
+};
