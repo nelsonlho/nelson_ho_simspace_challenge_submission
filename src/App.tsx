8da2cc670fd55de2d78377d2d8e59044e6895438
nelsonlho <nelsonlho@gmail.com>
2023-04-19 21:35:23 -0700

    onclick on breed working

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useRef } from 'react';
+import { useMemo, useRef, useState } from 'react';
 import { useFetch } from './common/hooks';
 import { LIMIT } from './common/constants';
 
@@ -16,25 +16,46 @@ function App() {
     ref: isComponentMounted,
   });
 
+  const [query, setQuery] = useState<string>('');
+  const [selectedBreed, setSelectedBreed] = useState<string | null>(null);
+
   const breeds = useMemo(() => {
     return data
       ? Object.keys(data.message)
+          .filter((breed) => breed.includes(query.toLocaleLowerCase()))
           .filter((_, index) => index < LIMIT)
           .sort((previous, latter) => (previous > latter ? 1 : -1))
       : null;
-  }, [data]);
+  }, [data, query]);
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const nextQuery = event.target.value ?? '';
+    setQuery(nextQuery);
+  };
 
   type BreedsType = {
     breeds: string[];
+    currentSelectedBreed: string | null;
+    setSelectedBreed: (breed: string | null) => void;
   };
 
-  const Breeds = ({ breeds }: BreedsType) => {
+  const Breeds = ({
+    breeds,
+    currentSelectedBreed,
+    setSelectedBreed,
+  }: BreedsType) => {
+    const handleClick = (breed: string) => setSelectedBreed(breed);
     return (
       <div className="container m-auto grid grid-cols-4 gap-1">
         {breeds.map((breed, index) => (
           <div
-            className="cursor-pointer capitalize bg-gray-200 hover:bg-gray-400 justify-center text-center my-1 mx-4 p-4 "
+            className={`cursor-pointer capitalize bg-gray-200 justify-center text-center my-1 mx-4 p-4 ${
+              currentSelectedBreed === breed
+                ? 'bg-blue-500 hover:bg-blue-400'
+                : 'hover:bg-gray-400'
+            }`}
             key={`${index}_${breed}`}
+            onClick={() => handleClick(breed)}
           >
             {breed}
           </div>
@@ -49,15 +70,23 @@ function App() {
         <div>Dogs!!</div>
         <div>
           <input
-            className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="text-black border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 0"
             placeholder="Search breed"
+            onChange={handleQueryChange}
+            value={query}
           />
         </div>
       </div>
       <div>
         {loading ? 'Loading...' : null}
         {error ? 'An error has occurred' : null}
-        {!loading && !error && breeds ? <Breeds breeds={breeds} /> : null}
+        {!loading && !error && breeds ? (
+          <Breeds
+            breeds={breeds}
+            currentSelectedBreed={selectedBreed}
+            setSelectedBreed={setSelectedBreed}
+          />
+        ) : null}
       </div>
     </>
   );
